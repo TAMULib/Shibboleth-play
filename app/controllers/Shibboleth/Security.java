b13d5ed1c0735f15c2e1d0adbf3d53840d6eb909
Scott Phillips <scott@scottphillips.com>
2011-11-23 16:07:43 -0600

    The very first pass at the Shibboleth Plugin

---
+++ app/controllers/Shibboleth/Security.java
@@ -0,0 +1,93 @@
+package controllers.Shibboleth;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import play.Play;
+import play.mvc.*;
+import play.data.validation.*;
+import play.libs.*;
+import play.utils.*;
+
+public class Security extends Controller {
+
+	/**
+	 * This method checks that a profile is allowed to view this page/method.
+	 * This method is called prior to the method's controller annotated with the
+	 * e method.
+	 * 
+	 * @param profile
+	 * @return true if you are allowed to execute this controller method.
+	 */
+	static boolean check(String profile) {
+		return true;
+	}
+
+	/**
+	 * This method returns the current connected username
+	 * 
+	 * @return
+	 */
+	static String connected() {
+		return session.get("shibboleth");
+	}
+
+	/**
+	 * Indicate if a user is currently connected
+	 * 
+	 * @return true if the user is connected
+	 */
+	static boolean isConnected() {
+		return session.contains("shibboleth");
+	}
+
+	/**
+	 * This method is called after a successful authentication. You need to
+	 * override this method if you with to perform specific actions (eg. Record
+	 * the time the user signed in)
+	 */
+	static void onAuthenticated() {
+	}
+
+	/**
+	 * This method is called before a user tries to sign off. You need to
+	 * override this method if you wish to perform specific actions (eg. Record
+	 * the name of the user who signed off)
+	 */
+	static void onDisconnect() {
+	}
+
+	/**
+	 * This method is called after a successful sign off. You need to override
+	 * this method if you wish to perform specific actions (eg. Record the time
+	 * the user signed off)
+	 */
+	static void onDisconnected() {
+	}
+
+	/**
+	 * This method is called if a check does not succeed. By default it shows
+	 * the not allowed page (the controller forbidden method).
+	 * 
+	 * @param profile
+	 */
+	static void onCheckFailed(String profile) {
+		forbidden();
+	}
+
+	protected static Object invoke(String m, Object... args) throws Throwable {
+		Class security = null;
+		List<Class> classes = Play.classloader
+				.getAssignableClasses(Security.class);
+		if (classes.size() == 0) {
+			security = Security.class;
+		} else {
+			security = classes.get(0);
+		}
+		try {
+			return Java.invokeStaticOrParent(security, m, args);
+		} catch (InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+}
---
+++ app/controllers/Shibboleth/Shibboleth.java
@@ -0,0 +1,193 @@
+package controllers.Shibboleth;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.internal.runners.statements.Fail;
+
+import play.Logger;
+import play.Play;
+import play.data.validation.Required;
+import play.libs.Crypto;
+import play.mvc.Before;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Http.Header;
+import play.mvc.Router;
+import play.utils.Java;
+
+
+/**
+ * Shibboleth Authentication for the Play! Framework.
+ * 
+ * @author Scott Phillips, http://www.scottphillips.com/
+ *
+ */
+public class Shibboleth extends Controller {
+
+	@Before(unless={"login", "authenticate", "logout"})
+	static void checkAccess() throws Throwable {
+		// Redirect to login
+		if(!session.contains("username")) {
+			login();
+		}
+
+		// Check authentication profiles
+		Check check = getActionAnnotation(Check.class);
+		if (check == null)
+			check = getControllerInheritedAnnotation(Check.class);
+
+		for(String profile : check.value()) {
+			boolean hasProfile = (Boolean)Security.invoke("check", profile);
+			if(!hasProfile) {
+				Security.invoke("onCheckFailed", profile);
+			}
+		}
+
+	}
+
+	
+	/**
+	 * Initiate a shibboleth login.
+	 */
+	public static void login() throws Throwable {
+
+		// Determine where the Shibboleth Login initiator is
+		String shibLogin = Play.configuration.getProperty("shib.login.url",null);
+		if (shibLogin == null)
+			shibLogin = request.getBase() + "/Shibboleth.sso/Login";
+		
+		// Append the target query string
+		shibLogin += "?target="+request.getBase();
+		shibLogin += Router.reverse("Shibboleth.authenticate").url;
+		
+		Logger.debug("Shib: Redirecting to Shibboleth login initiator: "+shibLogin);
+		
+		redirect(shibLogin);
+	}
+
+	/**
+	 * Authenticate the session after returning from Shibboleth. 
+	 */
+	public static void authenticate() throws Throwable {
+		
+		// 1. Log all headers received, if debugging
+		if (Logger.isDebugEnabled()) {
+			String log = "Shib: Recieved the following headers: \n";
+			for (String name : request.headers.keySet()) {
+				for (String value : request.headers.get(name).values) {
+					log += "    '"+name+"' = '"+value+"'\n";
+				}
+			}
+			Logger.debug(log);
+		}
+		
+		// TODO: Add check for special HTTP parameter from shibboleth for all connections
+		
+		// 2. Map each header to a session attribute
+		Map<String,String> attributeMap = getAttributeMap();
+		for (String attribute : attributeMap.keySet()) {
+			
+			String headerName = attributeMap.get(attribute);
+			Header headers = request.headers.get(headerName);
+			
+			if (headers == null) {
+				Logger.warn("Shib: Did not find header '"+headerName+"' for attribute '"+attribute+"'.");
+				continue;
+			}
+			
+			if (headers.values.size() > 1) {
+				Logger.warn("Shib: Recieved multiple '"+headerName+"' headers for attribute '"+attribute+"', picking the first one.");
+				continue;
+			}
+			
+			String value = headers.value();
+			
+			if (value == null) {
+				Logger.warn("Shib: Recieved header '"+headerName+"' for attribute '"+attribute+"', but it was null.");
+				continue;
+			}
+			
+			// Store on the session.
+			session.put(attribute, value);
+			Logger.debug("Shib: Recieved attribute, '"+attribute+"' = '"+value+"'");
+		}
+		
+		// Redirect to the original URL
+		redirectToOriginalURL();
+	}
+
+	/**
+	 * Logout of shibboleth, clear the session, and if configured
+	 * initiate a Shibboleth Logout.
+	 */
+	public static void logout() throws Throwable {
+		Security.invoke("onDisconnect");
+		session.clear();
+		Security.invoke("onDisconnected");
+		
+		String useLogout = Play.configuration.getProperty("shib.logout","false"); 
+		if ( useLogout.equalsIgnoreCase("true")) {
+			// Determine where the Shibboleth logout initiator is
+			String shibLogout = Play.configuration.getProperty("shib.logout.url",null);
+			if (shibLogout == null)
+				shibLogout = request.getBase() + "/Shibboleth.sso/Logout";
+			
+			String shibReturn = Play.configuration.getProperty("shib.logout.return",null);
+			if (shibReturn == null)
+				shibReturn = request.getBase() + "/";
+			
+			
+			// Append the target query string
+			shibLogout += "?return="+shibReturn;
+			
+			Logger.debug("Shib: Redirecting to Shibboleth logout initiator: "+shibLogout);
+		}
+		
+		String shibReturn = Play.configuration.getProperty("shib.logout.return",null);
+		if (shibReturn == null)
+			shibReturn = request.getBase() + "/";
+
+		redirect(shibReturn);
+	}
+
+
+	/**
+	 * Redirect to the original user's email
+	 */
+	static void redirectToOriginalURL() throws Throwable {
+		Security.invoke("onAuthenticated");
+		String url = flash.get("url");
+		if(url == null) {
+			url = "/";
+		}
+		redirect(url);
+	}
+	
+	
+	/**
+	 * @return The mapped session attributes from the Play! configuration. HashMap of Attribute names to HTTP Header names
+	 */
+	private static Map<String,String> getAttributeMap() {
+		Map<String,String> attributeMap = new HashMap<String,String>();
+		
+		Set<Object> keys = Play.configuration.keySet();
+		for (Object keyObj : keys) {
+			if (keyObj instanceof String) {
+				String key = (String) keyObj;
+				if (key.startsWith("shib.attribute")) {
+					String attribute = key.substring("shib.attribute".length());
+					String header = Play.configuration.getProperty(key);
+					
+					attributeMap.put(attribute, header);
+				}
+			}
+		}
+		
+		return attributeMap;
+	}
+
+}
