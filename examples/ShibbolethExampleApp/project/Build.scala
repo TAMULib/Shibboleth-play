3c6c5f2fbd88fa6b7b11948ecae74828e967a671
Scott Phillips <scott@scottphillips.com>
2012-04-15 13:37:08 -0500

    Added the shibboleth dependency and some temporary configuration parameters just to play around with.

--- examples/ShibbolethExampleApp/project/Build.scala
+++ examples/ShibbolethExampleApp/project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "play"    %    "shibboleth_2.9.1"    %    "1.0-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
